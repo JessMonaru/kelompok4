1ad516dae5f1eb8165b57ae1f6f825f6170861e6
Doni <ddsgaming19@gmail.com>
2023-05-17 13:54:38 +0700

    utsdoni

--- lib/page/detailgame_page.dart
+++ lib/page/detailgame_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tugas1pcs/model/game_model.dart';
 
+
 class detailgame extends StatelessWidget {
   gamemodel? game;
    detailgame({super.key,this.game});
@@ -9,26 +10,29 @@ class detailgame extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange,
         title: Text(game!.namagame!),
         ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            child: Image.asset(
-              game!.gambargame!,
-              fit: BoxFit.fill,),
-          ),
-          Text(game!.namagame!,
-          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-           Text(
-            game!.detailgame!,
-            textAlign: TextAlign.center,
-          style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-        ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Image.asset(
+                game!.gambargame!,
+                fit: BoxFit.fill,),
+            ),
+            Text(game!.namagame!,
+            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+             Text(
+              game!.detailgame!,
+              textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ]),
+      ),
     );
   }
 }
